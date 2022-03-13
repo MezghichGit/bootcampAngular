d8ba4c0337cae97da00ae5182d0f57313fcffb0f
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-03-13 17:30:07 +0100

    consommation de service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,11 @@ import { UsersService } from '../services/users.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  logo:string="https://smart-it-partner.com/wp-content/uploads/2021/06/logo.png";
   etudiants:string[]=[];
   mesPosts:Post[];
   nom:string="mon composant";
+  displayed:boolean=true;
   constructor(private serviceUser: UsersService, private servicePosts:PostsService) {
 
    }
@@ -28,6 +29,11 @@ export class HomeComponent implements OnInit {
       }
     );
 
+  }
+  display()
+  {
+    //alert("Merci bq Angular");
+    this.displayed = !this.displayed
   }
 
 }
